18fe4c0c28f98ac0935089550f1bcbef9c20bc4c
MailsonSousa88 <legionslmax86@gmail.com>
2025-08-11 09:19:39 -0300

    Versão final

--- script/index.ts
+++ script/index.ts
@@ -18,14 +18,18 @@ function voltarAoMenu(): void {
   }
 }
 
+function sobre(): void{
+  alert("Bem-vindo(a) ao Quiz Mania!\nEsse jogo foi criado pelos alunos do IFPI - campus Piripiri na disciplina de algoritimos e programação\nCriadores: F.Mailson, Carlos e Railson");
+  alert("O jogo oferece 4 temas diferentes para você escolher. Cada tema contém 10 perguntas, cada uma com 4 alternativas, das quais apenas uma é correta. Boa sorte e divirta-se!");
+}
 
-function jogarTema (temaEscolhido:Array<Pergunta>){
+function jogarTema(temaEscolhido:Array<Pergunta>){
   let pontuacao = 0; 
   let acertos = 0;
   for (let pergunta of temaEscolhido) {
     let resultado = pergunta.checaRespostaDoJogo();
 
-    if (resultado === "Voltar ao menu") {
+    if (resultado == "Voltar ao menu") {
       alert("Obrigado, até mais!");
       break;
     }else if(resultado == "Resposta correta"){
@@ -37,12 +41,20 @@ function jogarTema (temaEscolhido:Array<Pergunta>){
 
   alert(`Quantidade de acertos: ${acertos}/10\nPontuação total: ${pontuacao}`);
 }
+
+//Area dos botoes (TEMAS)
 let btnTemaGeral: HTMLButtonElement;
 btnTemaGeral = document.getElementById("btnTemaGeral") as HTMLButtonElement;
-
 btnTemaGeral.onclick = () => jogarTema(perguntaTemaGeral);//botao tema 1
 
 let btnTemaCartoon : HTMLButtonElement;
 btnTemaCartoon = document.getElementById("btnTemaCartoon") as HTMLButtonElement;
-
 btnTemaCartoon.onclick = () => jogarTema(perguntaTemaCartoon);//botao tema 2
+
+let btnTemaHerois: HTMLButtonElement;
+btnTemaHerois = document.getElementById("btnTemaHerois") as HTMLButtonElement;
+btnTemaHerois.onclick = () => jogarTema(perguntaTemaHerois); //botão tema 3
+
+let btnTemaPaises: HTMLButtonElement;
+btnTemaPaises = document.getElementById("btnTemaPaises") as HTMLButtonElement;
+btnTemaPaises.onclick = () => jogarTema(perguntaTemaPaises); //botão tema 4
--- script/temas.ts
+++ script/temas.ts
@@ -10,17 +10,17 @@ class Pergunta {
         }  
     //Metodo para checar as respostas dos modos
         checaRespostaDoJogo(): string{
-            let pergunta : number = Number(prompt(`${this.textoDaPergunta}\n0. Desistir\n${this.alternativasDaPergunta.join("\n")}`)!);
+            let perguntaChecada : number = Number(prompt(`${this.textoDaPergunta}\n0. Desistir\n${this.alternativasDaPergunta.join("\n")}`)!);
 
-            while(isNaN(pergunta) || pergunta < 0 || pergunta > 4){
+            while(isNaN(perguntaChecada) || perguntaChecada < 0 || perguntaChecada > 4){
                 alert("Opção incorreta!");
-                pergunta = Number(prompt(`${this.textoDaPergunta}\n0. Desistir\n${this.alternativasDaPergunta.join("\n")}`)!);
+                perguntaChecada = Number(prompt(`${this.textoDaPergunta}\n0. Desistir\n${this.alternativasDaPergunta.join("\n")}`)!);
             }
 
-            if(pergunta == 0){
+            if(perguntaChecada == 0){
                return "Voltar ao menu";
             }
-            else if(pergunta == this.respostasCorretas){
+            else if(perguntaChecada == this.respostasCorretas){
                 alert("Certa resposta! ✅")
                 return "Resposta correta";
             }
@@ -55,4 +55,30 @@ let perguntaTemaCartoon : Array<Pergunta> = [
     new Pergunta("Em 'Dragon Ball Z', qual é o verdadeiro nome do Goku?", ["1. Son Goku", "2. Kakarotto", "3. Vegeta", "4. Kaio"], 2),
     new Pergunta("Qual dessas personagens é uma princesa da Disney?", ["1. Mônica", "2. Emília", "3. Dora", "4. Ariel"], 4),
     new Pergunta("Em 'Apenas um Show', quais são os nomes dos dois personagens principais?", ["1. Gumball e Darwin", "2. Rick e Morty", "3. Mordecai e Rigby ", "4. Finn e Jake"], 3)
+];
+
+let perguntaTemaHerois : Array<Pergunta> = [
+    new Pergunta("Qual é o nome do martelo de Thor no Universo Marvel?", ["1. Mjolnir", "2. stormbreaker", "3. Gungnir", "4. Excalibur"], 1),
+    new Pergunta("Qual é o alter ego de Homem de Ferro?", ["1. Bruce Wayne", "2. Tony Stark", "3.  Steve Rogers", "4. Clarck Kent"], 2),
+    new Pergunta("Qual é o nome do vilão principal em 'Vingadores: Guerra Infinita'??", ["1. Ultron", "2. Thanos", "3. Loki", "4. Hela"], 2),
+    new Pergunta("Qual é o nome do reino de origem de Pantera Negra?", ["1. Asgard", "2. Wakanda", "3. Atlantis", "4. Sokovia"], 2),
+    new Pergunta("Qual super-herói é conhecido como o 'Cavaleiro das Trevas'?", ["1. Batman", "2. Superman", "3. Homem-Aranha", "4. Pantera Negra"], 1),
+    new Pergunta("Qual é o nome do grupo de super-heróis liderado por Charles Xavier?", ["1. Os Vingadores", "2. Os X-Men", "3. Os Guardiões da Galáxia", "4. A Liga da Justiça"], 2),
+    new Pergunta("Qual super-herói tem como inimigo o Duende Verde?", ["1. Homem-Aranha", "2. Batman", "3. Superman", "4. Flash "], 1),
+    new Pergunta("Qual é o nome da equipe de super-heróis que inclui Rocket e Groot?", ["1. Os Guardiões da Galáxia", "2. Os X-Men", "3. A Liga da Justiça ", "4. Vingadores"], 1),
+    new Pergunta("Qual é o nome verdadeiro de Capitão América?", ["1.Steve Rogers ", "2.  Bruce Banner", "3. Clark Kent ", "4. Peter"], 1),
+    new Pergunta("Qual é o nome do vilão que destrói Asgard em 'Thor: Ragnarok'?", ["1. Hela", "2. Loki", "3. Thanos", "4. Surtu"], 4),
+];
+
+let perguntaTemaPaises : Array<Pergunta> = [
+    new Pergunta("Quais os dois países da America do Sul que não fazem fronteira com o Brail?", ["1. Argentina & Venezuela", "2. Chile & Equador", "3. Bolivia & Uruguai", "4. Guiana & Suriname"], 2),
+    new Pergunta("Qual o menor país do mundo?", ["1. Nepal", "2. Vaticano", "3.  Uruguai", "4. México"], 2),
+    new Pergunta("Qual país mais populoso o mundo?", ["1. Estados Unidos", "2. Canadá", "3. Rússia", "4. Índia"], 4),
+    new Pergunta("Qual capital da Espanha?", ["1. Berlim", "2. Washington", "3. Madrid", "4. Buenos Aires"], 3),
+    new Pergunta("Qual país com maior extenção terrestre?", ["1. Brasil", "2. Índia", "3. Canadá", "4. Rússia"], 4),
+    new Pergunta("Qual país é conhecido pela sua Torre Eiffel?", ["1. Ítalia", "2. França", "3. Espanha", "4. Alemanha"], 2),
+    new Pergunta("Qual país é o maior exportador de café do mundo?", ["1. Brasil", "2. Estados Unidos", "3. Suiça", "4. China"], 1),
+    new Pergunta("Qual país é famoso por pizza e massa?", ["1. Argentina", "2. México", "3. Brasil", "4. Venezuela"], 2),
+    new Pergunta("Qual país é conhecido pela aurora boreal?", ["1. Islãndia", "2.  Noruega", "3. Finlândia", "4. Canadá"], 2),
+    new Pergunta("Qual país com o maior salario minimo do mundo?", ["1. Suiça", "2. Coreia do Sul", "3. Estados Unidos", "4. Canadá"], 1),
 ];
